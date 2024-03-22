c3aac436fd927c645eefb4310dd08ef0ae17d2a0
Matymboup <mboupmaty0@gmail.com>
2024-03-22 14:30:39 +0100

    Correction des erreurs 1,2,3 et 4

--- EhodVenteEnLigne/Models/ViewModels/ProductViewModel.cs
+++ EhodVenteEnLigne/Models/ViewModels/ProductViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
 
 namespace EhodBoutiqueEnLigne.Models.ViewModels
 {
@@ -7,14 +8,19 @@ namespace EhodBoutiqueEnLigne.Models.ViewModels
         [BindNever]
         public int Id { get; set; }
 
+
+        [Required(ErrorMessage = "Veiller renseigner le nom du produit")]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Details { get; set; }
 
+
+        [Required(ErrorMessage = "Veiller renseigner le stock du produit ")]
         public string Stock { get; set; }
 
+        [Required(ErrorMessage = "Veiller renseigner le prix du produit")]
         public string Price { get; set; }
     }
 }
